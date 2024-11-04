cd08c65a1fc932370fed4f28431ac901504339ed
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-04 22:42:03 +0100

    Delete MonumentData

--- src/monuments/types.ts
+++ src/monuments/types.ts
@@ -1,11 +1,3 @@
-export interface MonumentData {
-  name: string;
-  description: string;
-  imageUrl: string;
-  country: string;
-  city: string;
-}
-
 export interface MonumentStructure {
   name: string;
   description: string;
