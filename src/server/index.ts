20f375bb8c3ae9fc0c92d9e93519d7f9b3c0d40d
Erik Riquelme <erikone2311@gmail.com>
2024-11-06 22:47:52 +0100

    Add test for get method of  monumentsController

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,8 @@
 import express from "express";
-import MonumentsController from "../monuments/Monument/controllers/MonumentsController.js";
 import morgan from "morgan";
-import monuments from "../monuments/data/index.js";
 
 const app = express();
 
-const monumentsController = new MonumentsController(monuments);
-
 app.use(morgan("dev"));
-
-app.get("/monuments", monumentsController.get);
 
 export default app;
