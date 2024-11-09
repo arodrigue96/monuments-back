d4a72e84e45db712bb73f3155640f4f5a4b510ae
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-09 20:51:53 +0100

    Create handlePathError middleware

--- src/server/errors/middlewares.ts
+++ src/server/errors/middlewares.ts
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 import { type NextFunction, type Request, type Response } from "express";
-import type ServerError from "./ServerError/ServerError.js";
+import ServerError from "./ServerError/ServerError.js";
 
 export const handleGeneralError = (
   error: ServerError,
@@ -14,4 +14,14 @@ export const handleGeneralError = (
   res.status(error.statusCode ?? 500).json({
     error: error.statusCode ? error.message : "Server error",
   });
+};
+
+export const handlePathError = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction,
+): void => {
+  const error = new ServerError(404, "Endpoint not found");
+
+  next(error);
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import monumentsRouter from "../monument/router/monumentsRouter.js";
-import { handleGeneralError } from "./errors/middlewares.js";
+import { handleGeneralError, handlePathError } from "./errors/middlewares.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use("/monuments", monumentsRouter);
+
+app.use(handlePathError);
 
 app.use(handleGeneralError);
 
