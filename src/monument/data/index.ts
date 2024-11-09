702cdebd6cb47732bd0c09c0ed485e46cdcb1e80
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-09 22:38:11 +0100

    Delete images from monument data

--- src/monument/data/index.ts
+++ src/monument/data/index.ts
@@ -3,7 +3,7 @@ import Monument from "../Monument/Monument.js";
 const angkorWat = new Monument(
   "Angkor Wat",
   "Angkor Wat is a stunning temple complex in Siem Reap, Cambodia, built in the early 12th century. Originally dedicated to the Hindu god Vishnu, it is the largest religious monument in the world, covering 162.6 hectares. The temple's design symbolizes Mount Meru, with intricate bas-reliefs depicting mythological scenes. Transitioning to a Buddhist site over the centuries, Angkor Wat is now a UNESCO World Heritage Site and a symbol of Cambodian culture, attracting millions of visitors each year.",
-  "src/monument/images/angkor-wat.webp",
+  "https://elviajedejein.com/wp-content/uploads/2024/07/templos_angkor_megaguia.jpg",
   {
     country: "Cambodja",
     city: "Siem Reap",
@@ -13,7 +13,7 @@ const angkorWat = new Monument(
 const colosseum = new Monument(
   "Colosseum",
   "The Colosseum, also known as the Flavian Amphitheater, is an iconic elliptical amphitheater located in the heart of Rome, Italy. Construction began under Emperor Vespasian around 70 AD and was completed in 80 AD by his son, Titus. It was built as a grand venue for public spectacles, including gladiatorial contests and animal hunts, capable of seating approximately 65,000 spectators. Over the centuries, the Colosseum has endured significant damage from natural disasters and neglect, yet it remains one of the best-preserved ancient structures in the world. Today, it is a UNESCO World Heritage Site and one of Italy's most visited tourist attractions, drawing millions of visitors each year to witness its historical grandeur and architectural brilliance.",
-  "src/monument/images/colosseo.webp",
+  "https://acrobatadelcamino.com/wp-content/uploads/2024/06/como-visitar-coliseo-768x393.webp",
   {
     country: "Italy",
     city: "Rome",
