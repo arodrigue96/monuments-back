86b969d6438538d53912e83a77de1d7bce38e172
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-10 00:15:59 +0100

    Create cors middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import monumentsRouter from "../monument/router/monumentsRouter.js";
 import { handleGeneralError, handlePathError } from "./errors/middlewares.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan("dev"));
 
