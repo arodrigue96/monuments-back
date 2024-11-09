163bada71de7eb98986f0adc96e5fddace409152
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-09 20:35:07 +0100

    Add test coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import { createDefaultPreset, JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   verbose: true,
   rootDir: "src",
+  collectCoverageFrom: ["**/*.ts", "!server/startServer.ts", "!index.ts"],
+  coverageDirectory: "../coverage",
   ...createDefaultPreset(),
 };
 
